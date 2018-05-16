e63c84065d753093a219cd2851a3fc76b43bab3b
Student Student <student@student.student>
2018-05-16 17:19:02 +1000

    Fix Registration Page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,20 +1,14 @@
 @extends('master')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Register</div>
+                <h1>Register</h1>
 
-                <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
 
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">Name</label>
+                        <div class="form-group">
+                            <label for="name">Name:</label>
 
-                            <div class="col-md-6">
                                 <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
 
                                 @if ($errors->has('name'))
@@ -22,13 +16,11 @@
                                         <strong>{{ $errors->first('name') }}</strong>
                                     </span>
                                 @endif
-                            </div>
                         </div>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">E-Mail Address</label>
+                        <div class="form-group">
+                            <label for="email">E-Mail Address</label>
 
-                            <div class="col-md-6">
                                 <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
 
                                 @if ($errors->has('email'))
@@ -36,13 +28,11 @@
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
                                 @endif
-                            </div>
                         </div>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">Password</label>
+                        <div class="form-group">
+                            <label for="password">Password</label>
 
-                            <div class="col-md-6">
                                 <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
 
                                 @if ($errors->has('password'))
@@ -50,21 +40,17 @@
                                         <strong>{{ $errors->first('password') }}</strong>
                                     </span>
                                 @endif
-                            </div>
                         </div>
 
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">Confirm Password</label>
+                        <div class="form-group">
+                            <label for="password-confirm">Confirm Password</label>
 
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-                            </div>
+                            <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
                         </div>
 
-                        <div class="form-group row">
-                            <label for="auth" class="col-md-4 col-form-label text-md-right">Authorisation Level</label>
+                        <div class="form-group">
+                            <label for="auth">Authorisation Level</label>
 
-                            <div class="col-md-6">
                                 <input id="auth" type="text" class="form-control{{ $errors->has('auth') ? ' is-invalid' : '' }}" name="auth" required>
 
                                 @if ($errors->has('auth'))
@@ -72,20 +58,13 @@
                                         <strong>{{ $errors->first('auth') }}</strong>
                                     </span>
                                 @endif
-                            </div>
                         </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Register
-                                </button>
-                            </div>
+                        <div class="text-center">
+                            <button type="button" class="btn btn-danger col-sm-5" style="margin: 2.5%;">Clear</button>
+                            <button type="submit" class="btn btn-primary col-sm-5" style="margin: 2.5%;">Register</button>
                         </div>
                     </form>
                 </div>
-            </div>
-        </div>
-    </div>
 </div>
 @endsection
