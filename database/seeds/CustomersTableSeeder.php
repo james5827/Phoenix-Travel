f2aa75df6d1bf18ec4797d9a60a691a14c267bde
James O'Neill <jamesoneill5827@gmail.com>
2018-06-06 19:40:58 +1000

    Updated Customers Seeder

--- database/seeds/CustomersTableSeeder.php
+++ database/seeds/CustomersTableSeeder.php
@@ -16,12 +16,12 @@ class CustomersTableSeeder extends Seeder
         $password3 = md5("password" . "james@mail.com");
 
         DB::insert("INSERT INTO customers(customer_id, first_name, middle_initial, last_name, street_no, street_name, suburb, postcode, email, password, phone, authcustomer)
-                          VALUES(1, 'Fred', NULL, 'Smith', 500, 'Waverly Road', 'Chadstone', 3555, 'fred.Smith@hit.com','" . $password1 . "', NULL, 1);");
+                          VALUES(1, 'Fred', NULL, 'Smith', 500, 'Waverly Road', 'Chadstone', 3555, 'fred.Smith@hit.com','" . $password1 . "', NULL, true);");
 
         DB::insert("INSERT INTO customers(customer_id, first_name, middle_initial, last_name, street_no, street_name, suburb, postcode, email, password, phone, authcustomer)
-                          VALUES(2, 'William', 'B', 'Pitt', 200, 'St. Kilda Road', 'St. Kilda', 3147, 'bill.pitt@gail.com', '" . $password2 . "', 0351806451, 1);");
+                          VALUES(2, 'William', 'B', 'Pitt', 200, 'St. Kilda Road', 'St. Kilda', 3147, 'bill.pitt@gail.com', '" . $password2 . "', 0351806451, true);");
 
         DB::insert("INSERT INTO customers(customer_id, first_name, middle_initial, last_name, street_no, street_name, suburb, postcode, email, password, phone, authcustomer)
-                          VALUES(3, 'James', 'O', 'Neal', 100, 'Cardinia Rd', 'Hallam', 3100, 'james@mail.com', '" . $password3 . "', 4234566451, 1);");
+                          VALUES(3, 'James', 'O', 'Neal', 100, 'Cardinia Rd', 'Hallam', 3100, 'james@mail.com', '" . $password3 . "', 4234566451, true);");
     }
 }
